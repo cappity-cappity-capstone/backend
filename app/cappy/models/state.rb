08d839dd0dd6bc8328c83350d8b863286af7d1ae
Dan Osborne <djosborne10@gmail.com>
2015-01-28 16:34:25 -0500

    fixed constant upcase and multiline statement in test

--- app/cappy/models/state.rb
+++ app/cappy/models/state.rb
@@ -3,11 +3,11 @@ module Cappy
     # This model represents the state of a connected device
     class State < ActiveRecord::Base
       self.table_name = 'states'
-      valid_sources = %w(scheduled parent_left manual_override)
+      VALID_SOURCES = %w(scheduled parent_left manual_override)
 
       validates :device_id,  presence: true
       validates :state,      presence: true
-      validates :source,     presence: true, inclusion: { in: valid_sources }
+      validates :source,     presence: true, inclusion: { in: VALID_SOURCES }
       validates :created_at, presence: true
     end
   end
