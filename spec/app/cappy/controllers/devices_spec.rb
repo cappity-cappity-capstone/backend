4e358ce6d16c9d0b2d59c48bc55eb9f046ac79e6
Todd Lunter <tlunter@gmail.com>
2015-01-31 14:45:25 -0500

    Add new request specs

--- spec/app/cappy/controllers/devices_spec.rb
+++ spec/app/cappy/controllers/devices_spec.rb
@@ -136,4 +136,81 @@ describe Cappy::Controllers::Devices do
       end
     end
   end
+
+  describe 'PUT /devices/DEVICE_ID/watchdog/' do
+    context 'when the DEVICE_ID does not exist' do
+      it 'returns a "404 not found"' do
+        put '/devices/does_not_exist/watchdog/'
+
+        expect(last_response.status).to eq(404)
+      end
+    end
+
+    context 'when the DEVICE_ID exists' do
+      let(:device) { create(:lock) }
+      let(:device_id) { device.device_id }
+
+      before do
+        device.last_check_in = Time.mktime(0)
+        device.save
+      end
+
+      it 'updates the last_check_in time' do
+        expect {
+          put "/devices/#{device_id}/watchdog/"
+
+          expect(last_response.status).to eq(200)
+        }.to change {
+          device.tap(&:reload).last_check_in
+        }
+      end
+    end
+  end
+
+  describe 'GET /devices/DEVICE_ID/state/' do
+    context 'when the DEVICE_ID does not exist' do
+      it 'returns a "404 not found"' do
+        get '/devices/does_not_exist/state/'
+
+        expect(last_response.status).to eq(404)
+      end
+    end
+
+    context 'when the DEVICE_ID exists' do
+      let(:device) { create(:lock) }
+      let(:device_id) { device.device_id }
+      let!(:state) { create(:on_state, :scheduled, device: device) }
+
+      it 'reads the last state' do
+        get "/devices/#{device_id}/state/"
+
+        expect(last_response.status).to eq(200)
+        expect(JSON.parse(last_response.body)).to eq(state.as_json)
+      end
+    end
+  end
+
+  describe 'POST /devices/DEVICE_ID/state/' do
+    context 'when the DEVICE_ID does not exist' do
+      it 'returns a "404 not found"' do
+        post '/devices/does_not_exist/state/'
+
+        expect(last_response.status).to eq(404)
+      end
+    end
+
+    context 'when the DEVICE_ID exists' do
+      let(:device) { create(:lock) }
+      let(:device_id) { device.device_id }
+      let(:hash) { { state: '0.0', source: 'manual_override' } }
+
+      it 'creates a new last state' do
+        post "/devices/#{device_id}/state/", hash.to_json
+
+        expect(last_response.status).to eq(201)
+        expect(JSON.parse(last_response.body))
+          .to eq(device.states.last.as_json)
+      end
+    end
+  end
 end
