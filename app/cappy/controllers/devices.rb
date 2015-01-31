ca60e6200fccf27c7e21d557412901083538cba7
Todd Lunter <tlunter@gmail.com>
2015-01-31 14:24:58 -0500

    Add support for missing trailing slash

--- app/cappy/controllers/devices.rb
+++ app/cappy/controllers/devices.rb
@@ -3,32 +3,36 @@ module Cappy
     # This controller returns Cappy's version. It can be used on deploys as a
     # live-check URL.
     class Devices < Base
-      get '/devices/' do
+      # Basic crud support
+
+      get '/devices/?' do
         status 200
         Services::Devices.list.to_json
       end
 
-      post '/devices/' do
+      post '/devices/?' do
         status 201
         Services::Devices.create(parse_json(req_body)).to_json
       end
 
-      get '/devices/:device_id/' do |device_id|
+      get '/devices/:device_id/?' do |device_id|
         status 200
         Services::Devices.read(device_id).to_json
       end
 
-      put '/devices/:device_id/' do |device_id|
+      put '/devices/:device_id/?' do |device_id|
         status 200
         Services::Devices.update(device_id, parse_json(req_body)).to_json
       end
 
-      delete '/devices/:device_id/' do |device_id|
+      delete '/devices/:device_id/?' do |device_id|
         status 204
         Services::Devices.destroy(device_id)
       end
 
-      put '/devices/:device_id/watchdog' do |device_id|
+      # Device specific
+
+      put '/devices/:device_id/watchdog/?' do |device_id|
         status 200
         Services::Devices.update(device_id, last_check_in: Time.now.utc)
       end
