39853302b313eb7c86c7d5743b119ddecd135c57
Dan Osborne <djosborne10@gmail.com>
2015-02-06 15:21:37 -0500

    fixed states hash if no state is present

--- app/cappy/models/device.rb
+++ app/cappy/models/device.rb
@@ -16,7 +16,7 @@ module Cappy
           hash.delete('created_at')
           hash.delete('updated_at')
           hash['last_check_in'] = last_check_in.utc.iso8601 if last_check_in.present?
-          hash['state'] = states.any? ? states.last.as_json : nil
+          hash['state'] = states.last.as_json if states.any?
         end
       end
     end
