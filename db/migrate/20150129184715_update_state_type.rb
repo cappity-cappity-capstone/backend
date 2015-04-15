00e3baff477fd325143aff552d6602e9b26ff408
Todd Lunter <tlunter@gmail.com>
2015-04-15 17:26:47 -0400

    Fix tasks precision as well

--- db/migrate/20150129184715_update_state_type.rb
+++ db/migrate/20150129184715_update_state_type.rb
@@ -1,5 +1,5 @@
 class UpdateStateType < ActiveRecord::Migration
   def change
-    change_column :states, :state, :decimal, :precision => 10, :scale => 10
+    change_column :states, :state, :decimal, precision: 10, scale: 10
   end
 end
--- db/migrate/20150228124100_add_task_for_schedules.rb
+++ db/migrate/20150228124100_add_task_for_schedules.rb
@@ -2,7 +2,7 @@ class AddTaskForSchedules < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.integer :device_id, null: false
-      t.decimal :state, null: false
+      t.decimal :state, null: false, precision: 10, scale: 10
     end
 
     remove_column :schedules, :device_id
