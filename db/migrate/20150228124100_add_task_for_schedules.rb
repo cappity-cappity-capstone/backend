f78e3e2dbf4b36c6ff701ae2e12e5a9be61be09f
Todd Lunter <tlunter@gmail.com>
2015-04-15 19:32:30 -0400

    Wrong precisions -_-

--- db/migrate/20150228124100_add_task_for_schedules.rb
+++ db/migrate/20150228124100_add_task_for_schedules.rb
@@ -2,7 +2,7 @@ class AddTaskForSchedules < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.integer :device_id, null: false
-      t.decimal :state, null: false, precision: 10, scale: 10
+      t.decimal :state, null: false, precision: 2, scale: 1
     end
 
     remove_column :schedules, :device_id
